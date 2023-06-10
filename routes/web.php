a255dea70c5a7014de74bbd573ba49a2f1db46bd
SHUBHAM KUMAR <shri.shubham.kr@gmail.com>
2023-06-10 13:55:22 +0530

    add'

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeController;
+use App\Http\Controllers\ProductsController;
+ 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+ 
+Route::post('/session', [StripeController::class, 'session'])->name('session');
+Route::get('/success', [StripeController::class, 'success'])->name('success');
+Route::get('/cancel', [StripeController::class, 'cancel'])->name('cancel');
+ 
+Route::get('/', [ProductsController::class, 'index']);
+Route::get('cart', [ProductsController::class, 'cart'])->name('cart');
+Route::get('add-to-cart/{id}', [ProductsController::class, 'addToCart'])->name('add_to_cart');
+Route::patch('update-cart', [ProductsController::class, 'update'])->name('update_cart');
+Route::delete('remove-from-cart', [ProductsController::class, 'remove'])->name('remove_from_cart');
